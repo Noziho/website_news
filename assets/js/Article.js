b9c9f06e81d27e5e25973d6eeefec0be90fe9e35
Noziho <noah.decroix3@gmail.com>
2022-11-20 20:31:43 +0100

    print description on click

--- assets/js/Article.js
+++ assets/js/Article.js
@@ -3,19 +3,33 @@ let allNewsContainer = document.querySelector('.allNewsContainer');
 export function Article (title, resume, date, author, imageSrc) {
 
     this.title = title;
-    this.resume= resume;
+    this.resume = resume;
     this.date = date;
     this.author = author;
     this.imageSrc = imageSrc;
 
     this.createNews = function () {
 
-
         let newsContainer = document.createElement("div");
         newsContainer.className = "article";
 
+
+
+        newsContainer.addEventListener('mouseover', function () {
+            newsContainer.classList = 'overed';
+        })
+
+        newsContainer.addEventListener('mouseleave', function () {
+            newsContainer.classList = 'article';
+        })
+
+
+
         let articleTitle = document.createElement('h1');
         articleTitle.innerHTML = this.title;
+
+        let resumeText = document.createElement('p');
+        resumeText.innerHTML = this.resume;
 
         let articleImage = document.createElement('img');
         articleImage.src = this.imageSrc;
@@ -26,6 +40,17 @@ export function Article (title, resume, date, author, imageSrc) {
         let articleDate = document.createElement('p');
         articleDate.innerHTML = this.date;
 
+        newsContainer.addEventListener("click", function () {
+            newsContainer.classList = "overed";
+            let allNews = document.querySelectorAll('.article');
+            allNews.forEach(function (element){
+                element.style.display = "none";
+            })
+
+            newsContainer.append(resumeText);
+
+        });
+
         newsContainer.append(articleTitle);
         newsContainer.append(articleAuthor);
         newsContainer.append(articleDate);
@@ -35,7 +60,9 @@ export function Article (title, resume, date, author, imageSrc) {
         allNewsContainer.append(newsContainer);
 
 
-
     }
 
+    this.printOneArticle = function (description) {
+
+    }
 }
